da66442a8ba1fc0e86ccbdd02a78b4f64dba13cf
spaceraiders0 <spaceraiders@tutanota.com>
2021-03-16 20:43:38 +0000

    Fixed MergeSort.

--- algorithms/python/sorting/MergeSort.py
+++ algorithms/python/sorting/MergeSort.py
@@ -18,34 +18,24 @@ def MergeSort(unsorted: list):
     # Conquer
     sorted_list = []
     left_index, right_index = 0, 0
+    left_len, right_len = len(left), len(right)
 
-    # while len(left) != 0 and len(right) != 0:
-    #     first_left, first_right = left[0], right[0]
+    while len(left) != 0 and len(right) != 0:
+        first_left, first_right = left[0], right[0]
 
-    #     if first_left <= first_right:
-    #         sorted_list.append(first_left)
-    #         left = left[1:]
-    #     else:
-    #         sorted_list.append(first_right)
-    #         right = right[1:]
-
-    # # Clean up any remaining elements in either list.
-    # while len(left) > 0:
-    #     sorted_list.append(left.pop(0))
-
-    # while len(right) > 0:
-    #     sorted_list.append(right.pop(0))
-
-    while left_index < len(left) or right_index < len(right):
-        left_value, right_value = left[left_index], right[right_index]
-
-        if left_value > right_value:
-            sorted_list.append(left_value)
-            left_index += 1
+        if first_left <= first_right:
+            sorted_list.append(first_left)
+            left = left[1:]
         else:
+            sorted_list.append(first_right)
+            right = right[1:]
 
-            sorted_list.append(right_value)
-            right_index += 1
+    # Clean up any remaining elements in either list.
+    while len(left) > 0:
+        sorted_list.append(left.pop(0))
+
+    while len(right) > 0:
+        sorted_list.append(right.pop(0))
 
     return sorted_list
 
